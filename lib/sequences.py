0e3879750bb637846f0b63dad36b9e21d37ff17a
Abdisalan Ibrahim <crgmelo22@gmail.com>
2024-11-27 00:10:58 +0300

    .

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,8 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    if length <= 0:
+        print([])
+    else:
+        fibonacci = [0, 1]
+        while len(fibonacci) < length:
+            fibonacci.append(fibonacci[-1] + fibonacci[-2])
+        print(fibonacci[:length])
